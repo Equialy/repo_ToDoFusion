8f94a099a2592024c56c2b518093c9fbb392164b
Max <qiwima@gmail.com>
2024-09-12 19:41:23 +0300

    Add Styles forms

--- todo/forms.py
+++ todo/forms.py
@@ -1,6 +1,11 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Todo
-class ToDoForm(ModelForm):
+class ToDoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ['title', 'content', 'important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control','rows': 10, 'cols': 80,}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
--- todo/models.py
+++ todo/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=256)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_done = models.DateTimeField(null=True, blank=True)
-    time_update = models.DateTimeField(auto_now=True)
-    important = models.BooleanField(default=False)
+    title = models.CharField(max_length=256, verbose_name='Название заметки')
+    content = models.TextField(blank=True, verbose_name='Содержание')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_done = models.DateTimeField(null=True, blank=True, verbose_name='Время завершения')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+    important = models.BooleanField(default=False, verbose_name='Важно')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
