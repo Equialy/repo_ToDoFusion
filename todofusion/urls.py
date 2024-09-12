390714610f7445a3733afe2f691e920b3f77e044
Max <qiwima@gmail.com>
2024-09-13 01:28:36 +0300

    changes in settings statuc_url

--- todofusion/urls.py
+++ todofusion/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from todo import views
+from todofusion import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,3 +21,8 @@ urlpatterns = [
     path('todoread/<int:todo_pk>/delete/', views.tododelete, name='totoddelete'),
     path('', include('todo.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
